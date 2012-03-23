cad969fefe89be1ab61a7c1296c8fc26a843fa8e
Chris Ramakers <chris.ramakers@gmail.com>
2012-03-23 16:35:53 +0100

    Initial commit

---
+++ lib/App/Rest/Exception/ResourceNotFound.php
@@ -0,0 +1,7 @@
+<?php
+namespace App\Rest\Exception;
+
+class ResourceNotFound extends \Exception
+{
+
+}
---
+++ lib/App/Rest/ExceptionProcessor.php
@@ -0,0 +1,22 @@
+<?php
+namespace App\Rest;
+use App\Rest;
+
+class ExceptionProcessor
+{
+    public static function process(\Exception $e)
+    {
+        $response = new Response();
+        switch(get_class($e)){
+            case "App\Rest\Exception\AccessDenied":
+                return $response->setError(403)->setErrorMessage($e->getMessage() ?: "Access denied");
+                break;
+            case "App\Rest\Exception\ResourceNotFound":
+                return $response->setError(404)->setErrorMessage($e->getMessage() ?: "Resource not found");
+                break;
+            case "Exception":
+            default:
+                return $response->setError(500)->setErrorMessage($e->getMessage() ?: "Unknown error");
+        }
+    }
+}
