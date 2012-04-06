80f77f02afe9d90422e2ea2786b5653e2eb52987
Chris Ramakers <chris.ramakers@gmail.com>
2012-04-06 15:43:12 +0200

    Added GET coslist and cos detail

--- lib/App/Controller/Nug.php
+++ lib/App/Controller/Nug.php
@@ -27,10 +27,15 @@ class Nug
 
         $this->nugService = $this->app['service_nug'];
 
+        // Domain actions
         $this->controllers->get ('/domain/',             array($this, '_domainGetCollection'));
         $this->controllers->put ('/domain/',             array($this, '_domainCreate'));
         $this->controllers->get ('/domain/{domain_id}/', array($this, '_domainGetDetail'));
         $this->controllers->post('/domain/{domain_id}/', array($this, '_domainUpdate'));
+
+        // Class of service actions
+        $this->controllers->get('/cos/',                 array($this, '_cosGetCollection'));
+        $this->controllers->get('/cos/{cos_id}/',        array($this, '_cosGetDetail'));
 
         return $this->controllers;
     }
@@ -43,7 +48,7 @@ class Nug
      */
     public function _domainGetCollection()
     {
-        $domains = $this->nugService->getDomains();
+        $domains = $this->nugService->getDomainList();
         return new Rest\Response($domains);
     }
 
@@ -83,5 +88,30 @@ class Nug
         return new Rest\Response(array(
             'status' => "Create new domain"
         ));
+    }
+
+    /**
+     * Returns the response to GET /cos/
+     * which returns a collection of available "Classes of Service" from the ZCS server
+     *
+     * @return \App\Rest\Response
+     */
+    public function _cosGetCollection()
+    {
+        $cosList = $this->nugService->getCosList();
+        return new Rest\Response($cosList);
+    }
+
+    /**
+     * Returns the response to GET /cos/{cos_id}/
+     * which returns the details of a COS identified by $cos_id
+     * @param integer $cos_id
+     * @return \App\Rest\Response
+     * @throws \App\Rest\Exception\AccessDenied|\App\Rest\Exception\ResourceNotFound
+     */
+    public function _cosGetDetail($cos_id)
+    {
+        $cos = $this->nugService->getCos($cos_id);
+        return new Rest\Response($cos);
     }
 }
