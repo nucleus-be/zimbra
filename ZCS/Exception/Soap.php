cdc539371aec2ac17d3d38cc2391366d46d78de5
Chris Ramakers <chris.ramakers@gmail.com>
2012-04-13 16:02:16 +0200

    Only Zimbra Exceptions are now thrown

---
+++ ZCS/Exception/Soap.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * The exception is thrown when an error communicating with the soap webservice occurs
+ * When an error response from the webservice is returned on the other hand
+ * a \Zimbra\ZCS\Exception\Webservice exception will be thrown!
+ *
+ * @author LiberSoft <info@libersoft.it>
+ * @author Chris Ramakers <chris.ramakers@gmail.com>
+ * @license http://www.gnu.org/licenses/gpl.txt
+ */
+namespace Zimbra\ZCS\Exception;
+
+class Soap extends \Zimbra\ZCS\Exception
+{
+
+}
